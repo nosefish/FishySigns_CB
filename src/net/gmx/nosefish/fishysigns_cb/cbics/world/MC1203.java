f37ced1caed04d09866366a328bf5e9e301b1dc8
nosefish <nosefish@gmx.net>
2013-09-10 22:10:48 +0200

    lightning done right

--- src/net/gmx/nosefish/fishysigns_cb/cbics/world/MC1203.java
+++ src/net/gmx/nosefish/fishysigns_cb/cbics/world/MC1203.java
@@ -1,9 +1,6 @@
 package net.gmx.nosefish.fishysigns_cb.cbics.world;
 
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 import net.gmx.nosefish.fishylib.worldmath.FishyLocationInt;
@@ -15,7 +12,7 @@ import net.gmx.nosefish.fishysigns.signtools.FishyParser;
 import net.gmx.nosefish.fishysigns.signtools.FishyParser.Rule;
 import net.gmx.nosefish.fishysigns.signtools.FishyParser.Token;
 import net.gmx.nosefish.fishysigns.signtools.PatternLib;
-import net.gmx.nosefish.fishysigns.task.common.MultiLightningTask;
+import net.gmx.nosefish.fishysigns.task.common.CircleLightningTask;
 import net.gmx.nosefish.fishysigns_cb.cbics.CBBaseIC;
 
 public class MC1203 extends CBBaseIC {
@@ -26,17 +23,15 @@ public class MC1203 extends CBBaseIC {
 		null,
 		null };
 	
-	static Random rng = new Random();
-	
 	protected static final String key_RADIUS = "R";
 	protected static final String key_OFFSET = "XYZ";
 	protected static final String key_R_O_DELIMITER = "=";
 	protected static final String key_CHANCE = "P";
 	
-	protected static final double maxRadius = 5.0;
+	protected static int maxRadius = 5;
 	
-	protected volatile FishyLocationInt[] targets;
-	protected volatile double radius = 0.0;
+	protected volatile FishyLocationInt target;
+	protected volatile int radius = 0;
 	protected volatile double chance = 1.0;
 	
 	public MC1203(UnloadedSign sign) {
@@ -62,7 +57,7 @@ public class MC1203 extends CBBaseIC {
 	protected void constructOptionRules() {
 		super.constructOptionRules();
 		icOptionRules[2].add(new Rule(
-				PatternLib.pattern_POSITIVE_FLOAT, 
+				PatternLib.pattern_POSITIVE_INTEGER, 
 				new Token(key_RADIUS)));
 		icOptionRules[2].add(new Rule(
 				Pattern.compile("\\="), 
@@ -86,7 +81,7 @@ public class MC1203 extends CBBaseIC {
 	
 	@Override
 	protected synchronized void initializeIC() {
-		FishyLocationInt target = getLocation().addIntVector(BACK.toUnitIntVector());
+		target = getLocation().addIntVector(BACK.toUnitIntVector());
 		if (icOptions.containsKey(key_OFFSET)) {
 			String offsetString = icOptions.get(key_OFFSET).getValue();
 			FishyVectorInt offset = FishyParser.parseVectorInt(offsetString);
@@ -94,7 +89,7 @@ public class MC1203 extends CBBaseIC {
 		}
 		if (icOptions.containsKey(key_RADIUS)) {
 			String radiusString = icOptions.get(key_RADIUS).getValue();
-			radius = Double.parseDouble(radiusString);
+			radius = Integer.parseInt(radiusString);
 		}
 		if (icOptions.containsKey(key_CHANCE)) {
 			String chanceString = icOptions.get(key_CHANCE).getValue();
@@ -104,39 +99,14 @@ public class MC1203 extends CBBaseIC {
 				this.setLine(3, "1.0");
 			}
 		}
-		if (radius > 0.5) {
-			targets = findBlocksInRadius(target, Math.min(maxRadius, radius));
-		} else {
-			targets = new FishyLocationInt[]{target};
-		}
+		radius = Math.min(maxRadius, radius);
 	}
 
 	@Override
 	public void handleDirectInputChange(IOSignal oldS, IOSignal newS) {
 		if (isRisingEdge(oldS, newS, 0)) {
-			MultiLightningTask lightningTask = new MultiLightningTask(targets, chance);
+			CircleLightningTask lightningTask = new CircleLightningTask(target, radius, chance, true);
 			lightningTask.submit();
 		} 
 	}
-	
-	
-	public FishyLocationInt[] findBlocksInRadius(FishyLocationInt location, double radius) {
-		double radius2 = radius * radius;
-		Set<FishyLocationInt> result = new HashSet<FishyLocationInt>(4 * (int)radius2);
-		// check all locations in one quadrant of the surrounding square
-		for (int x = 0; x <= (int)radius + 1; x++) {
-			for (int z = 0; z <= (int)radius + 1; z++) {
-				FishyVectorInt vec = new FishyVectorInt(x, 0, z);
-				if (vec.lengthSquared() < radius2) {
-					result.add(location.addIntVector(vec));
-					// use symmetry
-					result.add(location.addIntVector(new FishyVectorInt(-x, 0, -z)));
-					result.add(location.addIntVector(new FishyVectorInt(-x, 0, z)));
-					result.add(location.addIntVector(new FishyVectorInt(x, 0, -z)));
-				}
-			}
-		}
-		return result.toArray(new FishyLocationInt[result.size()]);
-	}
-
 }
